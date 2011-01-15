523dcd8b77f83b75225a78e698e38efe9d04f2b0
Andreas Schempp <andreas@schempp.ch>
2011-01-15 12:55:02 +0000

    Code cleaning with regex search-replace. Removed unnecessary white-spaces and tabs. According to Contao coding style, empty lines should not contain any characters.

--- ContentAttributeLinkRepeater.php
+++ ContentAttributeLinkRepeater.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -41,72 +41,72 @@ class ContentAttributeLinkRepeater extends ContentElement
 	 */
 	protected function compile()
 	{
-		
+
 		//global $objPage;
 		$objAttributeData = $this->Database->prepare("SELECT name, option_list, use_alternate_source, list_source_table, list_source_field FROM tl_iso_attributes WHERE id=? AND is_filterable='1' AND (type='select' OR type='checkbox')")
 									  ->limit(1)
 									  ->execute($this->iso_filters);
-		
-		
+
+
 		if($objAttributeData->numRows < 1)
 		{
 			return '';
 		}
-		
+
 		if($objAttributeData->use_alternate_source==1)
 		{
 			$objLinkData = $this->Database->prepare("SELECT id, " . $objAttributeData->list_source_field . " FROM " . $objAttributeData->list_source_table)
 										  ->execute();
-			
+
 			if($objLinkData->numRows < 1)
 			{
 				return array();
 			}
-			
+
 			$arrLinkValues = $objLinkData->fetchAllAssoc();
-			
+
 			$filter_name = $objAttributeData->list_source_field;
-						
+
 			foreach($arrLinkValues as $value)
 			{
 				$arrLinkData[$value[$objAttributeData->id]] = $value[$objAttributeData->list_source_field];
-			
+
 			}
-			
+
 		}
 		else
 		{
 			$arrLinkValues = deserialize($objAttributeData->option_list);
-			
+
 			$filter_name = standardize($objAttributeData->name);
-			
+
 			foreach($arrLinkValues as $value)
 			{
 				$arrLinkData[$value['value']] = $value['label'];
-			
+
 			}
 		}
-		
+
 		if($blnSortAlpha) //just a temporary thing. Need to make this an option to set.
 		{
 			asort($arrLinkData);
 		}
-			
+
 		foreach($arrLinkData as $k=>$v)
 		{
-								
+
 			$arrLinks[] = array
 			(
 				'url'		=>	$this->url . '?' . $filter_name . '=' . $k, //$this->addToURL(),
 				'link'		=>	$v,
 				'title'		=> 	$v
 			);
-		}	
-			
-			
-		$this->Template->links = $arrLinks; 
-		
-		
+		}
+
+
+		$this->Template->links = $arrLinks;
+
+
 	}
 }
 
